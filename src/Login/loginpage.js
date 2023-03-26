46e365e479377ac39f582afb7d85941bcf2c78f1
StephenHoman <jakenesler@gmail.com>
2023-03-26 00:44:16 -0400

    modified:   src/Login/loginpage.js

--- src/Login/loginpage.js
+++ src/Login/loginpage.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import React, { useState } from 'react';
 import ColoredLine from './coloredline.js';
 import CreateAccount from './createAccount.js';
 import styles from './LoginPage.module.css';
 
-function LoginForm() {
+function LoginForm()  {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Email: ${email}, Password: ${password}`);
+    // later this will submit form
+
+  };
+
+
   return (
     <form className={styles['login-form']}>
       <label htmlFor="email"></label>
